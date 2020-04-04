fad0e2d6eed07933e30a7042d8bd28c68980c2c2
Thais <thaisfaria@cvc.ext.com>
2020-04-04 18:22:30 -0300

    including roboto from google fonts

--- components/base/styles.js
+++ components/base/styles.js
@@ -1,60 +1,62 @@
-import css from 'styled-jsx/css'
-import theme from '../theme';
+import css from "styled-jsx/css";
+import theme from "../theme";
 
 export default css.global`
-    * {
-        box-sizing: border-box;
+  @import url("https://fonts.googleapis.com/css?family=Roboto:400,500");
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    color: ${theme.palette.text};
+    font-family: ${theme.typography.primary};
+    font-size: ${theme.font.size.default};
+    margin: 0;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    list-style: none;
+  }
+  h1,
+  h2 {
+    margin: 0;
+  }
+  a {
+    color: ${theme.palette.text};
+    text-decoration: none;
+  }
+  .category-list .box {
+    align-items: flex-end;
+    display: flex;
+    height: 104px;
+    padding: ${theme.spacing(2)};
+  }
+  .page-question > .box {
+    padding: ${theme.spacing(4)} ${theme.spacing(5)};
+  }
+  .question-item .box {
+    cursor: pointer;
+    min-height: 60px;
+    padding: ${theme.spacing(2)};
+  }
+  .page-result .box {
+    max-width: 534px;
+    text-align: center;
+    padding-bottom: ${theme.spacing(4)};
+  }
+  @media ${theme.breakpoint.min.mobileL} {
+    .page-result .box {
+      margin: ${theme.spacing(7)} auto;
     }
-    body {    
-        color: ${theme.palette.text};
-        font-family: ${theme.typography.primary};
-        font-size: ${theme.font.size.default};
-        margin: 0;
-    }
-    ul {
-        margin: 0;
-        padding: 0;
-    }
-    li {
-        list-style: none;
-    }
-    h1, h2 {
-        margin: 0
-    }
-    a {
-        color: ${theme.palette.text};
-        text-decoration: none;
-    }
-    .category-list .box {
-        align-items: flex-end;
-        display: flex;
-        height: 104px;
-        padding: ${theme.spacing(2)};
-    }
-    .page-question >.box {
-        padding: ${theme.spacing(4)} ${theme.spacing(5)}
-    }
-    .question-item .box {
-        cursor: pointer;
-        min-height: 60px;
-        padding: ${theme.spacing(2)}
+  }
+  @media ${theme.breakpoint.max.mobileL} {
+    .page-question button {
+      width: 100%;
     }
     .page-result .box {
-        max-width: 534px;
-        text-align: center;
-        padding-bottom: ${theme.spacing(4)}
+      overflow: inherit;
     }
-    @media ${theme.breakpoint.min.mobileL} {
-        .page-result .box {
-            margin: ${theme.spacing(7)} auto;
-        }
-    } 
-    @media ${theme.breakpoint.max.mobileL} {
-        .page-question button {
-            width: 100%;
-        }
-        .page-result .box {
-            overflow: inherit;
-        }
-    }
-`
+  }
+`;
